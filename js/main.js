3b346ff0282fefd355b8bf2116cacca715dee47b
syykz <soyokazesutahamu@gmail.com>
2021-06-08 21:11:05 +0900

    Factoryで生成したインスタンスを扱う様に修正

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ import {PomodoroManager} from './PomodoroManager.js';
 import {MusicSessionFactory} from './MusicSessionFactory.js';
 
 var factory = new MusicSessionFactory();
-factory.create({src: "working.mp3", length: 1000 * 2});
-factory.create({src: "resting.mp3", length: 1000 * 4});
-factory.create({src: "working.mp3", length: 1000 * 2}); 
-factory.create({src: "resting.mp3", length: 1000 * 4});
-factory.create({src: "working.mp3", length: 1000 * 2});
+var musicSessions = [];
+musicSessions.push(factory.create({src: "working.mp3", length: 1000 * 2}));
+musicSessions.push(factory.create({src: "resting.mp3", length: 1000 * 4}));
+musicSessions.push(factory.create({src: "working.mp3", length: 1000 * 2})); 
+musicSessions.push(factory.create({src: "resting.mp3", length: 1000 * 4}));
+musicSessions.push(factory.create({src: "working.mp3", length: 1000 * 2}));
 
-var pomodoro = new PomodoroManager(document.getElementById("pomodoro"), factory.musicSessions);
+var pomodoro = new PomodoroManager(document.getElementById("pomodoro"), musicSessions);
 document.getElementById("play").addEventListener("click", ()=>{pomodoro.playPomodoro()});
 document.getElementById("pause").addEventListener("click", ()=>{pomodoro.pausePomodoro()});
