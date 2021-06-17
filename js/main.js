4a84fcfb940744ca0db50a1d7942b5d81cab187f
syykz <soyokazesutahamu@gmail.com>
2021-06-17 18:38:05 +0900

    微修正

--- js/main.js
+++ js/main.js
@@ -6,9 +6,16 @@ var musicSessions = [];
 
 factory.pushTmpAudioSrcFromPath("./0.mp3");
 factory.pushTmpAudioSrcFromPath("./1.mp3");
-factory.pushTmpAudioSrcFromPath("./2.mp3");
-musicSessions.push(factory.create(1000 * 20));
+musicSessions.push(factory.create(1000 * 2));
 
-var pomodoro = new PomodoroManager(document.getElementById("pomodoro"), musicSessions);
-document.getElementById("play").addEventListener("click", () => { pomodoro.playPomodoro() });
-document.getElementById("pause").addEventListener("click", () => { pomodoro.pausePomodoro() });
+factory.pushTmpAudioSrcFromPath("./2.mp3");
+factory.pushTmpAudioSrcFromPath("./3.mp3");
+musicSessions.push(factory.create(1000 * 2));
+
+factory.pushTmpAudioSrcFromPath("./4.mp3");
+factory.pushTmpAudioSrcFromPath("./5.mp3");
+musicSessions.push(factory.create(1000 * 5));
+
+window.syykzPomodoro = new PomodoroManager(document.getElementById("pomodoro"), musicSessions);
+document.getElementById("play").addEventListener("click", () => { window.syykzPomodoro.playPomodoro() });
+document.getElementById("pause").addEventListener("click", () => { window.syykzPomodoro.pausePomodoro() });
