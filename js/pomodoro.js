bc2286ddb72f9b17523569d40219904f60792dcf
syykz <soyokazesutahamu@gmail.com>
2021-10-24 20:35:11 +0900

    debug用、google auth確認

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -193,6 +193,42 @@ class Pomodoro extends React.Component {
             })
     }
 
+    oauthSignIn = () => {
+        var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+        // Create <form> element to submit parameters to OAuth 2.0 endpoint.
+        var form = document.createElement('form');
+        form.setAttribute('method', 'GET'); // Send as a GET request.
+        form.setAttribute('action', oauth2Endpoint);
+
+        let pro_url = 'https://syykz.net/api/users/auth/google_oauth2/callback';
+        let dev_url = 'http://localhost:3000/api/users/auth/google_oauth2/callback';
+
+        var params = {
+            'client_id': '119795716755-9cbu9bqmk9pl5e59hntnifceflcu105s.apps.googleusercontent.com',
+            'redirect_uri': pro_url,
+            'response_type': 'token',
+            'scope': 'https://www.googleapis.com/auth/drive.metadata.readonly',
+            'include_granted_scopes': 'true',
+            'state': 'pass-through value'
+        };
+
+        // Add form parameters as hidden input values.
+        for (var p in params) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'hidden');
+            input.setAttribute('name', p);
+            input.setAttribute('value', params[p]);
+            form.appendChild(input);
+        }
+
+        // Add form to page and submit it to open the OAuth 2.0 endpoint.
+        document.body.appendChild(form);
+
+        window.f = form;
+
+        form.submit();
+    }
+
     render() {
         return (
             <div>
@@ -205,6 +241,7 @@ class Pomodoro extends React.Component {
                     </div>
                 </nav>
                 <div className="container">
+                    <button onClick={this.oauthSignIn}>@dev ログイン</button>
                     {/* 表示系 */}
                     <div id="clock" className="center">{this.state.timer.toString()}</div>
                     {/* 操作系 */}
--- js/view/api.js
+++ js/view/api.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
+import process from 'process';
+
+let baseURL = 'http://www.syykz.net/api/'
+
+if (process.env.NODE_ENV !== 'production') {
+    baseURL = 'http://localhost:3000/';
+}
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: baseURL,
     withCredentials: true
 });
